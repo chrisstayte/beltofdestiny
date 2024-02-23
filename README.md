186ea0b56ee2326a1e7254bebe649c9cb6f58d58
Chris <ctstayte@gmaill.com>
2024-02-23 14:24:13 -0600

    Update Android and iOS package names and bundle identifiers
    - Updated the Android package name in `android/app/build.gradle` and `android/app/google-services.json` from "com.example.beltofdestiny" to "com.chrisstayte.beltofdestiny".
    - Updated the iOS bundle identifier in `ios/Runner.xcodeproj/project.pbxproj` and `ios/Runner/Info.plist` from "com.example.beltofdestiny" to "com.chrisstaytebeltofdestiny".
    
    Refactor RemoteConfig class initialization
    
    - Refactored the initialization of the RemoteConfig class in `lib/models/remote_config.dart`.
    - Added nullability checks for baseSpeed, maxSpeedIncreaseMultiplier, speedIncreasePer100Points, lowestTemp, and highestTemp.
    - If any of these values are null, default values are assigned.
    
    Update README.md
    
    - Made a minor update to the README.md file.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### Notes
 
-I stopped tracking `lib/firebase_options.dart` in the repo to keep my api key for firebase being so easily accessible.
+- I stopped tracking `lib/firebase_options.dart` in the repo to keep my api key for firebase being so easily accessible.
--- lib/models/remote_config.dart
+++ lib/models/remote_config.dart
@@ -1,15 +1,19 @@
 class RemoteConfig {
-  double baseSpeed = 320;
-  double maxSpeedIncreaseMultiplier = 2;
-  double speedIncreasePer100Points = 10;
-  double lowestTemp = 54.7;
-  double highestTemp = 57.2;
+  double baseSpeed;
+  double maxSpeedIncreaseMultiplier;
+  double speedIncreasePer100Points;
+  double lowestTemp;
+  double highestTemp;
 
   RemoteConfig({
     double? baseSpeed,
-    this.maxSpeedIncreaseMultiplier,
-    this.speedIncreasePer100Points,
-    this.lowestTemp,
-    this.highestTemp,
-  }) : baseSpeed = baseSpeed;
+    double? maxSpeedIncreaseMultiplier,
+    double? speedIncreasePer100Points,
+    double? lowestTemp,
+    double? highestTemp,
+  })  : baseSpeed = baseSpeed ?? 320,
+        maxSpeedIncreaseMultiplier = maxSpeedIncreaseMultiplier ?? 2,
+        speedIncreasePer100Points = speedIncreasePer100Points ?? 10,
+        lowestTemp = lowestTemp ?? 54.7,
+        highestTemp = highestTemp ?? 57.2;
 }
