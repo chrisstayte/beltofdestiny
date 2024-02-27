9251532fc1a8f2ec5a81c89e50b2fc319df5b62b
Chris Stayte <ctstayte@gmail.com>
2024-02-27 14:59:02 -0600

    updated game over screen

--- lib/screens/game/game_over_screen.dart
+++ lib/screens/game/game_over_screen.dart
@@ -14,60 +14,63 @@ class GameOverScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 400),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Game Over', style: Theme.of(context).textTheme.headlineLarge)
-                .animate(
-                  autoPlay: true,
-                  onComplete: (controller) => controller.repeat(),
-                )
-                .shimmer(
-                  color: Colors.yellow.shade800,
-                  duration: 1.5.seconds,
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Game Over',
+                      style: Theme.of(context).textTheme.headlineLarge)
+                  .animate(
+                    autoPlay: true,
+                    onComplete: (controller) => controller.repeat(),
+                  )
+                  .shimmer(
+                    color: Colors.yellow.shade800,
+                    duration: 1.5.seconds,
+                  ),
+              const Gap(20),
+              const Text('You got it next time!'),
+              const Gap(25),
+              ListTile(
+                title: Text('Score: '),
+                trailing: Text(
+                  '${_game.score.value}',
+                  style: TextStyle(color: Palette.eggPlant),
                 ),
-            const Gap(20),
-            const Text('You got it next time!'),
-            const Gap(25),
-            ListTile(
-              title: Text('Score: '),
-              trailing: Text(
-                '${_game.score.value}',
-                style: TextStyle(color: Palette.eggPlant),
               ),
-            ),
-            ListTile(
-              title: Text('Incinerated: '),
-              trailing: Text(
-                '${_game.garbageIncinerated}',
-                style: TextStyle(color: Palette.eggPlant),
+              ListTile(
+                title: Text('Incinerated: '),
+                trailing: Text(
+                  '${_game.garbageIncinerated}',
+                  style: TextStyle(color: Palette.eggPlant),
+                ),
               ),
-            ),
-            ListTile(
-              title: Text('Recycled: '),
-              trailing: Text(
-                '${_game.garbageRecycled}',
-                style: TextStyle(color: Palette.eggPlant),
+              ListTile(
+                title: Text('Recycled: '),
+                trailing: Text(
+                  '${_game.garbageRecycled}',
+                  style: TextStyle(color: Palette.eggPlant),
+                ),
               ),
-            ),
-            ListTile(
-              title: Text('Recycled Incorrectly: '),
-              trailing: Text(
-                '${_game.garbageRecycledIncorrectly}',
-                style: TextStyle(color: Palette.eggPlant),
+              ListTile(
+                title: Text('Recycled Incorrectly: '),
+                trailing: Text(
+                  '${_game.garbageRecycledIncorrectly}',
+                  style: TextStyle(color: Palette.eggPlant),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            NesButton(
-              onPressed: () {
-                context.go('/');
-              },
-              type: NesButtonType.error,
-              child: const Text('Quit'),
-            ),
-          ],
+              SizedBox(height: 16),
+              NesButton(
+                onPressed: () {
+                  context.go('/');
+                },
+                type: NesButtonType.error,
+                child: const Text('Quit'),
+              ),
+            ],
+          ),
         ),
       ),
     );
