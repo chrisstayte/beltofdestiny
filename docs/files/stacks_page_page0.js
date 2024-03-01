d67c1bd6f873db0d44514986649c23f41a755e43
Chris <ctstayte@gmaill.com>
2024-03-01 00:00:51 -0600

    updated website

--- docs/files/stacks_page_page0.js
+++ docs/files/stacks_page_page0.js
@@ -17,7 +17,7 @@ stacks.stacks_in_2 = (function(stack) {document.addEventListener('DOMContentLoad
 	// Give the body tag a class reflecting this so we can reference it in the CSS.
 	function WebpIsSupported(e){if(window.createImageBitmap){fetch("data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=").then(function(e){return e.blob()}).then(function(t){createImageBitmap(t).then(function(){e(!0)},function(){e(!1)})})}else e(!1)}WebpIsSupported(function(e){e?document.body.classList.add("webp-supported"):document.body.classList.add("webp-not-supported")});
 
-	console.log("Foundry 3 — v3.2023.07.17");
+	console.log("Foundry 3 — v3.2024.01.17");
 
 
 });
